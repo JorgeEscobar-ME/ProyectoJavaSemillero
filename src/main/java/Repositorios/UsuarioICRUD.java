66447e60ce5a3dd7f126a21207e0a332208db668
Jorge Alejandro Escobar Zúñiga <jorgealejandroescobar0@gmail.com>
2023-03-31 23:12:37 -0500

    Avance en UsuarioICRUD

--- src/main/java/Repositorios/UsuarioICRUD.java
+++ src/main/java/Repositorios/UsuarioICRUD.java
@@ -2,15 +2,14 @@ package Repositorios;
 
 import Entidades.Usuario;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UsuarioICRUD implements ICRUD{
+public class UsuarioICRUD implements ICRUD {
 
     private String cadenaConexion;
+
     public UsuarioICRUD() {
         try {
             DriverManager.registerDriver(new org.sqlite.JDBC());
@@ -33,34 +32,94 @@ public class UsuarioICRUD implements ICRUD{
     @Override
     public void guardar(Object objeto) {
         String cadenaConexion = "jdbc:sqlite:banco.db";
-        try (Connection conexion = DriverManager.getConnection(cadenaConexion)){
+        try (Connection conexion = DriverManager.getConnection(cadenaConexion)) {
             Usuario usuario = (Usuario) objeto;
-            String sentenciaSQL="INSERT INTO USUARIOS (NOMBRE, APELLIDO, CEDULA) VALUES ('" + usuario.getNombre() + "', '" + usuario.getApellido() + "', '" + usuario.getCedula() + "')";
+            String sentenciaSQL = "INSERT INTO USUARIOS (NOMBRE, APELLIDO, CEDULA) VALUES ('" + usuario.getNombre() + "', '" + usuario.getApellido() + "', '" + usuario.getCedula() + "')";
             Statement sentencia = conexion.createStatement();
             sentencia.execute(sentenciaSQL);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-
     @Override
     public void eliminar(String identificador) {
+        String cadenaConexion = "jdbc:sqlite:banco.db";
+        try (Connection conexion = DriverManager.getConnection(cadenaConexion)) {
+            String sentenciaSql = "DELETE FROM Usuarios WHERE cedula = '" + identificador + "';";
+            Statement sentencia = conexion.createStatement();
+            sentencia.execute(sentenciaSql);
+        } catch (SQLException e) {
+            System.err.println("Error de conexión: " + e);
+        } catch (Exception e) {
+            System.err.println("Error " + e.getMessage());
+        }
 
     }
-
     @Override
     public void actualizar(Object objeto) {
+        String cadenaConexion = "jdbc:sqlite:banco.db";
+        try (Connection conexion = DriverManager.getConnection(cadenaConexion)) {
+            Usuario usuarios = (Usuario) objeto;
+            String sentenciaSql = "UPDATE Usuarios SET nombre = '" + usuarios.getNombre() + "', apellido = '"
+                    + usuarios.getApellido() + "', cedula = '" + usuarios.getCedula() + "';";
+
+            Statement sentencia = conexion.createStatement();
+            sentencia.execute(sentenciaSql);
+        } catch (SQLException e) {
+            System.err.println("Error de conexión: " + e);
+        } catch (Exception e) {
+            System.err.println("Error " + e.getMessage());
+        }
 
     }
-
     @Override
-    public Object buscar(String identificador) {
+    public Usuario buscar(String identificador) {
+        String cadenaConexion = "jdbc:sqlite:banco.db";
+        try (Connection conexion = DriverManager.getConnection(cadenaConexion)) {
+            String sentenciaSQL = "SELECT * FROM Usuarios WHERE CEDULA = ?";
+            PreparedStatement sentencia = conexion.prepareStatement(sentenciaSQL);
+            sentencia.setString(1, identificador);
+            ResultSet resultadoConsulta = sentencia.executeQuery();
+            if (resultadoConsulta != null && resultadoConsulta.next()) {
+                String nombre = resultadoConsulta.getString("NOMBRE");
+                String apellido = resultadoConsulta.getString("APELLIDO");
+                String cedulaResultado = resultadoConsulta.getString("CEDULA");
+
+                return new Usuario(nombre, apellido, cedulaResultado);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
     public List<?> listar() {
+        List<Usuario> usuariosList = new ArrayList<Usuario>();
+        String cadenaConexion = "jdbc:sqlite:banco.db";
+        try (Connection conexion = DriverManager.getConnection(cadenaConexion)) {
+            String sentenciaSql = "SELECT * FROM USUARIOS";
+            PreparedStatement sentencia = conexion.prepareStatement(sentenciaSql);
+            ResultSet resultadoConsulta = sentencia.executeQuery();
+
+            if (resultadoConsulta != null) {
+                while (resultadoConsulta.next()) {
+                    Usuario usuarios;
+                    String nombre = resultadoConsulta.getString("NOMBRE");
+                    String apellido = resultadoConsulta.getString("APELLIDO");
+                    String cedula = resultadoConsulta.getString("CEDULA");
+                    usuarios = new Usuario(nombre, apellido, cedula);
+                    usuariosList.add(usuarios);
+                }
+                return usuariosList;
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error de conexión: " + e);
+        }
         return null;
+
     }
 
     @Override
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,14 +6,16 @@ import Repositorios.TransaccionesICRUD;
 import Repositorios.UsuarioICRUD;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class main {
     public static void main(String[] args) {
-        UsuarioICRUD usuarioicrud= new UsuarioICRUD();
-//        Usuario persona = new Usuario("Jorge", "Escobar", "1053868118");
+        UsuarioICRUD usuarioicrud = new UsuarioICRUD();
+        Usuario persona = new Usuario("Jorge", "Escobar", "1053868118");
 //        usuarioicrud.guardar(persona);
         TransaccionesICRUD transaccionesICRUD = new TransaccionesICRUD();
 
@@ -26,17 +28,28 @@ public class main {
         DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String fechaActualFormateada = fechaActual.format(formatoFecha);
 
-// Obtener la hora actual
+        // Obtener la hora actual
         LocalTime horaActual = LocalTime.now();
 
-// Formatear la hora en formato HH:mm:ss
+        // Formatear la hora en formato HH:mm:ss
         DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
         String horaActualFormateada = horaActual.format(formatoHora);
 
-        Transacciones transacciones = new Transacciones(fechaActualFormateada, horaActualFormateada, "deposito", 5000, "10", "Ahorros" );
-//        transaccionesICRUD.guardar(transacciones);
+        Transacciones transacciones = new Transacciones(fechaActualFormateada, horaActualFormateada, "deposito", 5000, "10", "Ahorros");
+        //        transaccionesICRUD.guardar(transacciones);
         Cuentas cuenta = new Cuentas("26", 1500, "Ahorros", "101");
         CuentasICRUD cuentasICRUD = new CuentasICRUD();
-        cuentasICRUD.guardar(cuenta);
+        //cuentasICRUD.guardar(cuenta);
+
+//        usuarioicrud.eliminar("1053868118");
+        Usuario persona1 = new Usuario("Jorge", "Escobar", "9953868118");
+//        usuarioicrud.actualizar(persona1);
+        Usuario persona2 = usuarioicrud.buscar("9953868118");
+        System.out.println(persona2.getApellido());
+        List<?> lista = new ArrayList<>();
+        lista = usuarioicrud.listar();
+        Usuario primerusuario = (Usuario) lista.get(0);
+
+        System.out.println(primerusuario.getApellido());
     }
 }
